bf25f957626479de94a21eb950fee4551fc6afd8
ok-sana-code <108321942+ok-sana-code@users.noreply.github.com>
2022-07-01 16:50:39 +0300

    Added relevant icon

--- src/app.js
+++ src/app.js
@@ -28,6 +28,7 @@ function showTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dataElement = document.querySelector("#data");
+  let iconElement = document.querySelector("#icon");
 
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].main;
@@ -35,9 +36,14 @@ function showTemperature(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dataElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].main);
 }
 let apiKey = "860949625d49936c19736b773a04ad68";
-let city = "tokio";
+let city = "oslo";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(showTemperature);
