d763b6311fa1dac410113aee402d7bdb9e7a23ce
ok-sana-code <108321942+ok-sana-code@users.noreply.github.com>
2022-07-01 15:26:54 +0300

    Updated the time

--- src/app.js
+++ src/app.js
@@ -1,18 +1,43 @@
+function formatDate(timestamp) {
+  let date = new Date();
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours === `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    hours === `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function showTemperature(response) {
   let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = response.data.name;
   let descriptionElement = document.querySelector("#description");
-  descriptionElement.innerHTML = response.data.weather[0].main;
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
   let humidityElement = document.querySelector("#humidity");
-  humidityElement.innerHTML = response.data.main.humidity;
-  console.log(response.data.wind.speed);
   let windElement = document.querySelector("#wind");
+  let dataElement = document.querySelector("#data");
+
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].main;
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  dataElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 let apiKey = "860949625d49936c19736b773a04ad68";
-let city = "london";
+let city = "tokio";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(showTemperature);
